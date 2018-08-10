89de53e4ccea06813e39cf0d73f28e52c765e7c2
Xuefeng <i@oiuv.cn>
2018-08-10 16:35:21 +0800

    modify page footer

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="footer">
     <div class="container">
         <p class="pull-left">
-            由 <a href="http://weibo.com/u/1837553744?is_hot=1" target="_blank">Summer</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+            由 <a href="https://github.com/oiuv/mud.ren" target="_blank">秋林拾叶</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
         </p>
 
         <p class="pull-right"><a href="mailto:{{ setting('contact_email') }}">联系我们</a></p>
