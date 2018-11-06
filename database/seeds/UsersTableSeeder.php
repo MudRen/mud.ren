b511d358ed3ed0dffd80be8f0c8e9198e9451abe
oiuv <i@oiuv.cn>
2018-11-06 23:43:20 +0800

    更新seed用户图像

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,27 +12,21 @@ class UsersTableSeeder extends Seeder
 
         // 头像假数据
         $avatars = [
-            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/s5ehp11z6s.png?imageView2/1/w/200/h/200',
-            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/Lhd1SHqu86.png?imageView2/1/w/200/h/200',
-            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/LOnMrqbHJn.png?imageView2/1/w/200/h/200',
-            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/xAuDMxteQy.png?imageView2/1/w/200/h/200',
-            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/ZqM7iaP4CR.png?imageView2/1/w/200/h/200',
-            'https://fsdhubcdn.phphub.org/uploads/images/201710/14/1/NDnzMutoxX.png?imageView2/1/w/200/h/200',
-        ];
+            'https://via.placeholder.com/200x200/333',
+            'https://via.placeholder.com/200x200/369',
+            'https://via.placeholder.com/200x200/393/000',
+            'https://via.placeholder.com/200x200/693/000',];
 
         // 生成数据集合
-        $users = factory(User::class)
-                        ->times(10)
-                        ->make()
-                        ->each(function ($user, $index)
-                            use ($faker, $avatars)
-        {
-            // 从头像数组中随机取出一个并赋值
-            $user->avatar = $faker->randomElement($avatars);
-        });
+        $users = factory(User::class)->times(10)->make()->each(function ($user, $index) use ($faker, $avatars) {
+                // 从头像数组中随机取出一个并赋值
+                $user->avatar = $faker->randomElement($avatars);
+            });
 
         // 让隐藏字段可见，并将数据集合转换为数组
-        $user_array = $users->makeVisible(['password', 'remember_token'])->toArray();
+        $user_array = $users->makeVisible([
+            'password',
+            'remember_token'])->toArray();
 
         // 插入到数据库中
         User::insert($user_array);
