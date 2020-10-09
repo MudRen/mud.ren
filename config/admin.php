1548476a0f69ff1ec6789a554889cbbca179d50e
oiuv <i@oiuv.cn>
2020-10-09 21:53:02 +0800

    update laravel to 8.0

--- config/admin.php
+++ config/admin.php
@@ -95,7 +95,7 @@ return [
     | If your page is going to be accessed via https, set it to `true`.
     |
     */
-    'https' => env('ADMIN_HTTPS', true),
+    'https' => env('ADMIN_HTTPS', false),
 
     /*
     |--------------------------------------------------------------------------
@@ -138,6 +138,7 @@ return [
         'excepts' => [
             'auth/login',
             'auth/logout',
+            '_handle_action_',
         ],
     ],
 
