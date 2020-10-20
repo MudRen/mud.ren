87d1b4dec2eecb2b2524c2cb3453aebcf4c48cfa
oiuv <i@oiuv.cn>
2020-10-20 21:37:16 +0800

    更新玩家列表样式

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,11 @@
         @yield('content')
     </div>
 
+    <div class="bg-dark shadow-6 py-2">
+        <div class="container text-center text-white-60">
+            <a href="http://beian.miit.gov.cn" class="text-light" target="_blank">京ICP备13031296号-4</a>
+        </div>
+    </div>
     @livewireScripts
 </body>
 </html>
--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -1,6 +1,13 @@
-<div class="text-center">
+<div class="text-center alert alert-info">
     <h1> {{ $message }} </h1>
 </div>
+<nav aria-label="breadcrumb">
+    <ol class="breadcrumb">
+        <li class="breadcrumb-item"><a href="https://bbs.mud.ren/">社区</a></li>
+        <li class="breadcrumb-item"><a href="https://bbs.mud.ren/nodes/6/">炎黄</a></li>
+        <li class="breadcrumb-item active" aria-current="page">英雄榜</li>
+    </ol>
+</nav>
 <div class="table-responsive">
 <table class="table table-striped table-dark table-hover table-sm">
     <thead>
@@ -25,23 +32,26 @@
 
     <tbody>
 @foreach ($users as $user)
-    @continue(!data_get(json_decode($user, true), 'id'))
+    @php
+        $dbase = json_decode($user, true);
+    @endphp
+    @continue(!data_get($dbase, 'id'))
     <tr>
-        <td>{{ data_get(json_decode($user, true), 'name') }}（{{ data_get(json_decode($user, true), 'id') }}）</td>
-        <!-- <td>{{ data_get(json_decode($user, true), 'gender') }}</td> -->
-        <!-- <td>{{ data_get(json_decode($user, true), 'character') }}</td> -->
-        <td>{{ data_get(json_decode($user, true), 'age') }}</td>
-        <td>{{ data_get(json_decode($user, true), 'title') }}</td>
-        <td>{{ data_get(json_decode($user, true), 'family.master_name') }}</td>
-        <td>{{ data_get(json_decode($user, true), 'max_qi') }}</td>
-        <td>{{ data_get(json_decode($user, true), 'max_jing') }}</td>
-        <td>{{ data_get(json_decode($user, true), 'max_neili') }}</td>
-        <td>{{ data_get(json_decode($user, true), 'max_jingli') }}</td>
-        <td>{{ data_get(json_decode($user, true), 'combat_exp') }}</td>
-        <td>{{ data_get(json_decode($user, true), 'weiwang') }}</td>
-        <td>{{ data_get(json_decode($user, true), 'shen') }}</td>
-        <td>{{ data_get(json_decode($user, true), 'combat.MKS') }}</td>
-        <td>{{ data_get(json_decode($user, true), 'combat.dietimes') }}</td>
+        <td>{{ data_get($dbase, 'name') }}（{{ data_get($dbase, 'id') }}）</td>
+        <!-- <td>{{ data_get($dbase, 'gender') }}</td> -->
+        <!-- <td>{{ data_get($dbase, 'character') }}</td> -->
+        <td>{{ data_get($dbase, 'age') }}</td>
+        <td>{{ data_get($dbase, 'title') }}</td>
+        <td>{{ data_get($dbase, 'family.master_name') }}</td>
+        <td>{{ data_get($dbase, 'max_qi') }}</td>
+        <td>{{ data_get($dbase, 'max_jing') }}</td>
+        <td>{{ data_get($dbase, 'max_neili') }}</td>
+        <td>{{ data_get($dbase, 'max_jingli') }}</td>
+        <td>{{ data_get($dbase, 'combat_exp') }}</td>
+        <td>{{ data_get($dbase, 'weiwang') }}</td>
+        <td>{{ data_get($dbase, 'shen') }}</td>
+        <td>{{ data_get($dbase, 'combat.MKS') }}</td>
+        <td>{{ data_get($dbase, 'combat.dietimes') }}</td>
     </tr>
 @endforeach
     </tbody>
