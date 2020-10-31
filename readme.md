83e1c261e75fd0ca09441151f0a831a79b6ca871
oiuv <i@oiuv.cn>
2020-10-31 16:49:25 +0800

    add mta

--- readme.md
+++
@@ -1,5 +0,0 @@
-## MUDREN网站
-
-* 官网：https://mud.ren
-* 社区：https://bbs.mud.ren
-* 百科：https://mud.wiki
--- resources/views/layouts/livewire.blade.php
+++ resources/views/layouts/livewire.blade.php
@@ -16,18 +16,30 @@
 <body>
 @include('layouts.navbar')
 
-    <div class="container">
-        @yield('content')
-    </div>
+<div class="container">
+@yield('content')
+</div>
 
-    <div class="bg-dark shadow-6 py-2">
-        <div class="container text-center text-white-60">
-            <a href="http://beian.miit.gov.cn" class="text-light" target="_blank">京ICP备13031296号-4</a>
-        </div>
+<div class="bg-dark shadow-6 py-2">
+    <div class="container text-center text-white-60">
+        <a href="http://beian.miit.gov.cn" class="text-light" target="_blank">京ICP备13031296号-4</a>
     </div>
-    @livewireScripts
+</div>
+@livewireScripts
 <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.0/dist/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
+<script>
+    var _mtac = {};
+    (function() {
+        var mta = document.createElement("script");
+        mta.src = "//pingjs.qq.com/h5/stats.js?v2.0.4";
+        mta.setAttribute("name", "MTAH5");
+        mta.setAttribute("sid", "500724040");
+
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(mta, s);
+    })();
+</script>
 </body>
 </html>
